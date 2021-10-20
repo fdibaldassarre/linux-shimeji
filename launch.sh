b13bcdd7f9ad8fef216ea78cff40c123b060f60d
Francesco Di Baldassarre <f.dibaldassarre@Raphael>
2021-10-20 21:34:15 +0200

    Restore original launch script

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export LD_LIBRARY_PATH="/usr/lib/jvm/default-java/lib/"
-java -classpath /usr/share/linux-shimeji/shimeji.jar -Xmx1000m com.group_finity.mascot.Main >/tmp/linux-shimeji.log 2>&1
+java -classpath target/linux-shimeji-0.0.1-SNAPSHOT-jar-with-dependencies.jar -Xmx1000m com.group_finity.mascot.Main
